f0c8f34efeea53326b4f13e50a1610b1fcca6602
mikewehr <wehr@uoregon.edu>
2017-07-11 16:15:15 -0700

    update

--- LED_laser_monitor/LED_laser_monitor.ino
+++ LED_laser_monitor/LED_laser_monitor.ino
@@ -5,21 +5,27 @@ doesn't go negative. (function generator is bipolar)
 */
 
 const int  AnalogInPin = A1;    // the pin that the function generator signal is attached to
-const int LEDOutPin = 1;       // pin to drive LED
-const int threshold = 10;       // 10 AD units is ~50 mV
+const int LEDOutPin = 0;       // pin to drive LED
+const int LEDOutPin2 = 13;       // pin to drive LED
+const int threshold = 200;       // 10 AD units is ~50 mV
+int LEDValue=0;
 
 void setup() {
   // initialize the LED as an output:
   pinMode(LEDOutPin, OUTPUT);
+  pinMode(LEDOutPin2, OUTPUT);
 }
 
 void loop() {
+
   // read the value from the sensor:
   LEDValue = analogRead(AnalogInPin);
   if (LEDValue>threshold) {
     digitalWrite(LEDOutPin, HIGH);
+    digitalWrite(LEDOutPin2, HIGH);
   }
   else if (LEDValue<=threshold) {
     digitalWrite(LEDOutPin, LOW);
+    digitalWrite(LEDOutPin2, LOW);
   }
-
+}
