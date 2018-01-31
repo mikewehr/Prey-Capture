f63e14cd0b7a0d0fcbbfde45738a1d502c8a2168
Mike <wehr@uoregon.edu>
2018-01-31 09:31:51 -0800

    starting groupdata analysis

--- preycapturebatch.m
+++ preycapturebatch.m
@@ -1,9 +1,19 @@
 %batch process prey capture files
 
 files=preycapturefilelist;
-cd ('C:\Users\lab\Desktop\826 mice bonsai')
+analysis_plots_dir= 'C:\Users\lab\Desktop\826 mice bonsai';
+if ismac    
+    analysis_plots_dir= strrep(analysis_plots_dir, '\', '/');
+    analysis_plots_dir= strrep(analysis_plots_dir, 'C:', '/Volumes/C');
+end
+cd(analysis_plots_dir)
 delete analysis_plots.ps
+       fprintf('\nAnalyzing %d files...', length(files))
 
 for i=1:length(files)
    AnalyzeBonsaiTrackingData( files(i).datapath, files(i).start_frame, files(i).stop_frame)
+   
+   if ~mod(i,10)
+       fprintf('\nfile %d/%d', i, length(files))
+   end
 end
